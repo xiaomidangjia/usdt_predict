8754d10c6b897253f1964fbb51acdc6c451e4e0f
xiaomidangjia <68535633+xiaomidangjia@users.noreply.github.com>
2023-06-10 22:58:33 +0800

    Add files via upload

--- send_picture.py
+++ send_picture.py
@@ -3,7 +3,7 @@ bot = telegram.Bot(token='6219784883:AAE3YXlXvxNArWJu-0qKpKlhm4KaTSHcqpw')
 bot.sendDocument(chat_id='-840309715', document=open('/root/usdt_predict/out/未来24小时BTC价格趋势预测.png', 'rb'))
 
 from dingtalkchatbot.chatbot import DingtalkChatbot
-webhook = 'https://oapi.dingtalk.com/robot/send?access_token=69d2f134c31ced0426894ed975f29b519c1a8bd163a808840ef5812c5a0477a1'
+webhook = 'https://oapi.dingtalk.com/robot/send?access_token=a9789d2739819eab19b07dcefe30df3fcfd9f815bf198ced54c71c557f09e7d9'
 from qiniu import Auth, put_file, etag
 def gmt_img_url(key=None,local_file=None,**kwargs):
     # refer:https://developer.qiniu.com/kodo/sdk/1242/python
